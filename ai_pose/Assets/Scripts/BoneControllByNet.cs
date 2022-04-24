8882f0c87277aed7bd5efe0c986142b9aac85916
EarlyAnt <54763755@qq.com>
2022-04-24 19:37:09 +0800

    1.BoneControllByNet.cs脚本，增加盆骨中心点的虚拟骨骼点，并以其计算其他骨骼点的localPosition。 2.下一步，解决数据传输不完整的问题，以减少错误的发生。

--- ai_pose/Assets/Scripts/BoneControllByNet.cs
+++ ai_pose/Assets/Scripts/BoneControllByNet.cs
@@ -23,6 +23,7 @@ public class BoneControllByNet : MonoBehaviour
     private bool serverRunning { get; set; }
     private bool clientRunning { get; set; }
     private Queue<Action> taskList = new Queue<Action>();
+    private const string BODY = "BODY";
     /************************************************Unity方法与事件***********************************************/
     private void Start()
     {
@@ -64,7 +65,7 @@ public class BoneControllByNet : MonoBehaviour
             }
             catch (Exception ex)
             {
-                Debug.LogErrorFormat("BoneControllByNet.Update->set bone position error: {0}", ex);
+                //Debug.LogErrorFormat("BoneControllByNet.Update->set bone position error: {0}", ex);
             }
         }
     }
@@ -82,7 +83,7 @@ public class BoneControllByNet : MonoBehaviour
     {
         if (this.gizmosDatas == null || !this.gizmosDatas.Enable || this.gizmosDatas.Datas.Count == 0)
             return;
-        
+
         foreach (var data in this.gizmosDatas.Datas)
         {
             for (int i = 0; i < data.Points.Count; i++)
@@ -107,6 +108,8 @@ public class BoneControllByNet : MonoBehaviour
         while (this.boneRoot.childCount > 0)
             GameObject.DestroyImmediate(this.boneRoot.GetChild(0).gameObject);
 
+        this.bones[0].CreateBone(this.boneRoot, BODY, this.boneScale, false);
+
         TextAsset ta = Resources.Load<TextAsset>(string.Format("CSV/data"));
         Debug.LogFormat("LoadCsvData->data file name: {0}", ta.name);
         List<string> lines = new List<string>(ta.text.Split('\n'));
@@ -114,7 +117,7 @@ public class BoneControllByNet : MonoBehaviour
         string line = lines[0];
         string[] columnHeaders = line.Split('#');
         for (int i = 0; i < columnHeaders.Length; i++)
-            this.bones[i].CreateBone(this.boneRoot, columnHeaders[i], this.boneScale, false);
+            this.bones[i + 1].CreateBone(this.boneRoot, columnHeaders[i], this.boneScale, false);
     }
     private void OnServerReceiveMessage(string message)
     {
@@ -122,39 +125,85 @@ public class BoneControllByNet : MonoBehaviour
 
         if (string.IsNullOrEmpty(message))
         {
-            Debug.LogWarning("BoneControllByNet.OnServerReceiveMessage->parameter 'message' is null or empty");
+            //Debug.LogWarning("BoneControllByNet.OnServerReceiveMessage->parameter 'message' is null or empty");
             return;
         }
 
-        string[] positions = message.Split('#');
-        if (positions.Length == 0)
+        List<string> positions = new List<string>(message.Split('#'));
+        if (positions.Count == 0)
         {
-            Debug.LogWarning("BoneControllByNet.OnServerReceiveMessage->none position data");
+            //Debug.LogWarning("BoneControllByNet.OnServerReceiveMessage->none position data");
             return;
         }
 
-        foreach (string position in positions)
+        try
         {
-            string[] datas = position.Split(',');
-            if (datas.Length != 4)
+            string leftHip = positions.Find(t => t.ToLower().Contains("left_hip"));
+            string rightHip = positions.Find(t => t.ToLower().Contains("right_hip"));
+
+            if (string.IsNullOrEmpty(leftHip) || string.IsNullOrEmpty(rightHip))
+                return;
+
+            BoneData leftHipData = this.GetBoneData(leftHip);
+            BoneData rightHipData = this.GetBoneData(rightHip);
+
+            Vector3 hipPos = (rightHipData.Position - leftHipData.Position) / 2;
+            BodyBone hipBone = this.bones.Find(t => t.Name == BODY);
+            this.taskList.Enqueue(() => hipBone.SetPosition(hipPos));
+
+            foreach (string position in positions)
             {
-                Debug.LogWarning("BoneControllByNet.OnServerReceiveMessage->invalid position data" + (datas.Length > 0 ? string.Format(", bone name: {0}", datas[0]) : ""));
-                continue;
+                BoneData boneData = this.GetBoneData(position);
+                BodyBone bodyBone = this.bones.Find(t => t.Name == boneData.BoneName);
+                if (bodyBone == null)
+                {
+                    //Debug.LogErrorFormat("BoneControllByNet.OnServerReceiveMessage->can not find the bone[{0}]", bodyBone.Name);
+                    continue;
+                }
+
+                this.taskList.Enqueue(() =>
+                {
+                    Vector3 localPosition = bodyBone.Bone.InverseTransformPoint(boneData.Position);
+                    bodyBone.SetPosition(new Vector3(localPosition.x * this.rate.x, localPosition.y * this.rate.y, localPosition.z * this.rate.z));
+                });
             }
+        }
+        catch (Exception ex)
+        {
+            //Debug.LogWarning(ex);
+        }
+    }
+
+    private BoneData GetBoneData(string positionData)
+    {
+        try
+        {
+            string[] datas = positionData.Split(',');
+            if (datas.Length != 4)
+                throw new Exception("BoneControllByNet.GetBoneData->invalid position data" + (datas.Length > 0 ? string.Format(", bone name: {0}", datas[0]) : ""));
 
             string bone = datas[0];
             float x = float.Parse(datas[1]);
             float y = float.Parse(datas[2]);
             float z = float.Parse(datas[3]);
-
-            BodyBone bodyBone = this.bones.Find(t => t.Name == bone);
-            if (bodyBone == null)
-            {
-                Debug.LogErrorFormat("BoneControllByNet.OnServerReceiveMessage->can not find the bone[{0}]", bone);
-                continue;
-            }
-
-            this.taskList.Enqueue(() => bodyBone.SetPosition(new Vector3(x * this.rate.x, y * this.rate.y, z * this.rate.z)));
+            return new BoneData(bone, x, y, z);
         }
+        catch (Exception ex)
+        {
+            //Debug.LogErrorFormat("BoneControllByNet.GetBoneData->error: {0}", ex);
+            return new BoneData("", 0, 0, 0);
+        }
+    }
+}
+
+public class BoneData
+{
+    public string BoneName { get; set; }
+    public Vector3 Position { get; set; }
+
+    public BoneData(string boneName, float x, float y, float z)
+    {
+        this.BoneName = boneName;
+        this.Position = new Vector3(x, y, z);
     }
 }
