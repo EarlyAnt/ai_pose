8c61af168bea3de5cc1ec4364181902150162b32
EarlyAnt <54763755@qq.com>
2022-04-25 16:16:26 +0800

    1.LocalPosition.cs脚本，通过cube2的位置计算cube1的世界坐标，再用cube1的位置计算cube2和cube3的本地坐标，验证是否可行。结论是，不能这样倒推，cube1作为根节点是不能移动的，否则所有的子节点会随之移动。想想当时做FPS时，头显和手环也只是定位头和手的位置和角度，身体的位置是靠手环上的方向键控制的。

--- ai_pose/Assets/Test/LocalPosition/LocalPosition.cs
+++ ai_pose/Assets/Test/LocalPosition/LocalPosition.cs
@@ -11,15 +11,16 @@ public class LocalPosition : MonoBehaviour
 
     void Start()
     {
-        Debug.LogFormat("position->club1: {0}, cube2: {1}, cube3: {2}", cube1.position, cube2.position, cube3.position);
-        Debug.LogFormat("localPosition->club1: {0}, cube2: {1}, cube3: {2}", cube1.localPosition, cube2.localPosition, cube3.localPosition);
-        Debug.LogFormat("transfer->club1: {0}, {1}", cube1.localPosition, cube1.InverseTransformPoint(cube1.position));
-        Debug.LogFormat("transfer->club2: {0}, {1}", cube2.localPosition, cube1.InverseTransformPoint(cube2.position));
-        Debug.LogFormat("transfer->club3: {0}, {1}", cube3.localPosition, cube1.InverseTransformPoint(cube3.position));
+        Debug.LogFormat("position->club1: {0}, cube2: {1}, cube3: {2}", this.cube1.position, this.cube2.position, this.cube3.position);
+        Debug.LogFormat("localPosition->club1: {0}, cube2: {1}, cube3: {2}", this.cube1.localPosition, this.cube2.localPosition, this.cube3.localPosition);
+        Debug.LogFormat("transfer->club1: {0}, {1}", this.cube1.localPosition, this.cube1.InverseTransformPoint(this.cube1.position));
+        Debug.LogFormat("transfer->club2: {0}, {1}", this.cube2.localPosition, this.cube1.InverseTransformPoint(this.cube2.position));
+        Debug.LogFormat("transfer->club3: {0}, {1}", this.cube3.localPosition, this.cube1.InverseTransformPoint(this.cube3.position));
     }
 
     void Update()
     {
-        
+        this.cube1.position = this.cube2.position - new Vector3(10, 0, 0);
+        this.cube3.localPosition = this.cube1.InverseTransformPoint(this.cube3.position);
     }
 }
